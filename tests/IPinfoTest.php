10db103a8eed13fabdc49a87305296e75926af89
Uman Shahzad <uman@mslm.io>
2020-11-12 15:26:53 +0500

    composer fix-style

--- tests/IPinfoTest.php
+++ tests/IPinfoTest.php
@@ -10,78 +10,78 @@ class IPinfoTest extends TestCase
 {
     public function testAccessToken()
     {
-      $access_token = '123';
-      $client = new IPinfo($access_token);
-      $this->assertSame($access_token, $client->access_token);
+        $access_token = '123';
+        $client = new IPinfo($access_token);
+        $this->assertSame($access_token, $client->access_token);
     }
 
     public function testDefaultCountries()
     {
-      $client = new IPinfo();
-      $this->assertSame('United States', $client->countries['US']);
-      $this->assertSame('France', $client->countries['FR']);
+        $client = new IPinfo();
+        $this->assertSame('United States', $client->countries['US']);
+        $this->assertSame('France', $client->countries['FR']);
     }
 
     public function testCustomCache()
     {
-      $access_token = 'this is a fake access token';
-      $cache = 'this is a fake cache';
-      $client = new IPinfo($access_token, ['cache' => $cache]);
-      $this->assertSame($cache, $client->cache);
+        $access_token = 'this is a fake access token';
+        $cache = 'this is a fake cache';
+        $client = new IPinfo($access_token, ['cache' => $cache]);
+        $this->assertSame($cache, $client->cache);
     }
 
     public function testDefaultCacheSettings()
     {
-      $client = new IPinfo();
-      $this->assertSame(IPinfo::CACHE_MAXSIZE, $client->cache->maxsize);
-      $this->assertSame(IPinfo::CACHE_TTL, $client->cache->ttl);
+        $client = new IPinfo();
+        $this->assertSame(IPinfo::CACHE_MAXSIZE, $client->cache->maxsize);
+        $this->assertSame(IPinfo::CACHE_TTL, $client->cache->ttl);
     }
 
     public function testCustomCacheSettings()
     {
-      $access_token = 'this is a fake access token';
-      $settings = ['cache_maxsize' => 100, 'cache_ttl' => 11];
-      $client = new IPinfo($access_token, $settings);
-      $this->assertSame($settings['cache_maxsize'], $client->cache->maxsize);
-      $this->assertSame($settings['cache_ttl'], $client->cache->ttl);
+        $access_token = 'this is a fake access token';
+        $settings = ['cache_maxsize' => 100, 'cache_ttl' => 11];
+        $client = new IPinfo($access_token, $settings);
+        $this->assertSame($settings['cache_maxsize'], $client->cache->maxsize);
+        $this->assertSame($settings['cache_ttl'], $client->cache->ttl);
     }
 
     public function testFormatDetailsObject()
     {
-      $test_details = [
+        $test_details = [
         'country' => 'US',
         'loc' => '123,567'
-      ];
+        ];
 
-      $handler = new IPinfo();
-      $details = $handler->formatDetailsObject($test_details);
+        $handler = new IPinfo();
+        $details = $handler->formatDetailsObject($test_details);
 
-      $this->assertEquals($test_details['country'], $details->country);
-      $this->assertEquals('United States', $details->country_name);
-      $this->assertEquals($test_details['loc'], $details->loc);
-      $this->assertEquals('123', $details->latitude);
-      $this->assertEquals('567', $details->longitude);
+        $this->assertEquals($test_details['country'], $details->country);
+        $this->assertEquals('United States', $details->country_name);
+        $this->assertEquals($test_details['loc'], $details->loc);
+        $this->assertEquals('123', $details->latitude);
+        $this->assertEquals('567', $details->longitude);
     }
 
     public function testBuildHeaders()
     {
-      $token = '123abc';
+        $token = '123abc';
 
-      $handler = new IPinfo($token);
-      $headers = $handler->buildHeaders();
+        $handler = new IPinfo($token);
+        $headers = $handler->buildHeaders();
 
-      $this->assertArrayHasKey('headers', $headers);
-      $headers = $headers['headers'];
-      $this->assertEquals("IPinfoClient/PHP/1.0", $headers['user-agent']);
-      $this->assertEquals("application/json", $headers['accept']);
-      $this->assertEquals("Bearer $token", $headers['authorization']);
+        $this->assertArrayHasKey('headers', $headers);
+        $headers = $headers['headers'];
+        $this->assertEquals("IPinfoClient/PHP/1.0", $headers['user-agent']);
+        $this->assertEquals("application/json", $headers['accept']);
+        $this->assertEquals("Bearer $token", $headers['authorization']);
     }
 
     public function testBadIP()
     {
-      $ip = "fake_ip";
-      $handler = new IPinfo();
-      $this->expectException(IPinfoException::class);
-      $handler->getDetails($ip);
+        $ip = "fake_ip";
+        $handler = new IPinfo();
+        $this->expectException(IPinfoException::class);
+        $handler->getDetails($ip);
     }
 }
