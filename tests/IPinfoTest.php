7f5db20ca4db37d4926d4bee0d3b89a3fcac0672
Uman Shahzad <uman@mslm.io>
2020-11-13 14:32:00 +0500

    add a real test case that hits live api

--- tests/IPinfoTest.php
+++ tests/IPinfoTest.php
@@ -10,9 +10,9 @@ class IPinfoTest extends TestCase
 {
     public function testAccessToken()
     {
-        $access_token = '123';
-        $client = new IPinfo($access_token);
-        $this->assertSame($access_token, $client->access_token);
+        $tok = '123';
+        $client = new IPinfo($tok);
+        $this->assertSame($tok, $client->access_token);
     }
 
     public function testDefaultCountries()
@@ -24,9 +24,9 @@ class IPinfoTest extends TestCase
 
     public function testCustomCache()
     {
-        $access_token = 'this is a fake access token';
+        $tok = 'this is a fake access token';
         $cache = 'this is a fake cache';
-        $client = new IPinfo($access_token, ['cache' => $cache]);
+        $client = new IPinfo($tok, ['cache' => $cache]);
         $this->assertSame($cache, $client->cache);
     }
 
@@ -39,9 +39,9 @@ class IPinfoTest extends TestCase
 
     public function testCustomCacheSettings()
     {
-        $access_token = 'this is a fake access token';
+        $tok = 'this is a fake access token';
         $settings = ['cache_maxsize' => 100, 'cache_ttl' => 11];
-        $client = new IPinfo($access_token, $settings);
+        $client = new IPinfo($tok, $settings);
         $this->assertSame($settings['cache_maxsize'], $client->cache->maxsize);
         $this->assertSame($settings['cache_ttl'], $client->cache->ttl);
     }
@@ -53,21 +53,64 @@ class IPinfoTest extends TestCase
             'loc' => '123,567'
         ];
 
-        $handler = new IPinfo();
-        $details = $handler->formatDetailsObject($test_details);
+        $h = new IPinfo();
+        $res = $h->formatDetailsObject($test_details);
 
-        $this->assertEquals($test_details['country'], $details->country);
-        $this->assertEquals('United States', $details->country_name);
-        $this->assertEquals($test_details['loc'], $details->loc);
-        $this->assertEquals('123', $details->latitude);
-        $this->assertEquals('567', $details->longitude);
+        $this->assertEquals($test_details['country'], $res->country);
+        $this->assertEquals('United States', $res->country_name);
+        $this->assertEquals($test_details['loc'], $res->loc);
+        $this->assertEquals('123', $res->latitude);
+        $this->assertEquals('567', $res->longitude);
     }
 
     public function testBadIP()
     {
         $ip = "fake_ip";
-        $handler = new IPinfo();
+        $h = new IPinfo();
         $this->expectException(IPinfoException::class);
-        $handler->getDetails($ip);
+        $h->getDetails($ip);
+    }
+
+    public function testLookup()
+    {
+        $tok = getenv('IPINFO_TOKEN');
+        if (!$tok) {
+            $this->markTestSkipped('IPINFO_TOKEN env var required');
+        }
+
+        $h = new IPinfo($tok);
+        $ip = "8.8.8.8";
+        $res = $h->getDetails($ip);
+
+        $this->assertEquals($res->ip, '8.8.8.8');
+        $this->assertEquals($res->hostname, 'dns.google');
+        $this->assertEquals($res->city, 'Mountain View');
+        $this->assertEquals($res->region, 'California');
+        $this->assertEquals($res->country, 'US');
+        $this->assertEquals($res->country_name, 'United States');
+        $this->assertEquals($res->loc, '37.4056,-122.0775');
+        $this->assertEquals($res->latitude, '37.4056');
+        $this->assertEquals($res->longitude, '-122.0775');
+        $this->assertEquals($res->postal, '94043');
+        $this->assertEquals($res->timezone, 'America/Los_Angeles');
+        $this->assertEquals($res->asn['asn'], 'AS15169');
+        $this->assertEquals($res->asn['name'], 'Google LLC');
+        $this->assertEquals($res->asn['domain'], 'google.com');
+        $this->assertEquals($res->asn['route'], '8.8.8.0/24');
+        $this->assertEquals($res->asn['type'], 'business');
+        $this->assertEquals($res->company['name'], 'Google LLC');
+        $this->assertEquals($res->company['domain'], 'google.com');
+        $this->assertEquals($res->company['type'], 'business');
+        $this->assertEquals($res->privacy['vpn'], FALSE);
+        $this->assertEquals($res->privacy['proxy'], FALSE);
+        $this->assertEquals($res->privacy['tor'], FALSE);
+        $this->assertEquals($res->privacy['hosting'], FALSE);
+        $this->assertEquals($res->abuse['address'], 'US, CA, Mountain View, 1600 Amphitheatre Parkway, 94043');
+        $this->assertEquals($res->abuse['country'], 'US');
+        $this->assertEquals($res->abuse['email'], 'network-abuse@google.com');
+        $this->assertEquals($res->abuse['name'], 'Abuse');
+        $this->assertEquals($res->abuse['network'], '8.8.8.0/24');
+        $this->assertEquals($res->abuse['phone'], '+1-650-253-0000');
+        $this->assertEquals($res->domains['ip'], '8.8.8.8');
     }
 }
